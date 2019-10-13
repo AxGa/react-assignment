d1a336799b71f86dd298ea023cb3b477baaeec30
Achilleas Galatsidas <galatsidas@gmail.com>
2019-10-13 23:48:03 +0300

    Add wells plot

--- src/earthnet/Wellbore.js
+++ src/earthnet/Wellbore.js
@@ -5,6 +5,8 @@ import WellsList from '../layouts/components/Lists/WellsList';
 import LogsList from '../layouts/components/Lists/LogsList';
 import FormationsList from '../layouts/components/Lists/FormationsList';
 import EsaLogo from '../EsaLogo';
+import store from "../store/index";
+import { WellsPlot } from '../layouts/components/Plot/WellsPlot';
 
 const styles = theme => ({
   button: { marginTop: theme.spacing(3) },
@@ -18,26 +20,41 @@ const styles = theme => ({
     '& svg': {
       width: '30%'
     }
+  },
+  plotContainer:{
+    height: '100%',
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 });
 const useStyles = makeStyles(styles);
 
 export default function Wellbore() {
-	const classes = useStyles();
-	return (
-	<Dashboard>
-		<Grid container spacing={1} >
-			<Grid item xs={12} md={5} container spacing={2}>
-	  			<WellsList/>
-	  			<LogsList/>
-	  			<FormationsList/>
-				</Grid>
-				<Grid item xs={12} md={7}>
-	          <div className={classes.logoContainer}>
-	            <EsaLogo />
-	          </div>
-	        </Grid>
-		</Grid>
-	</Dashboard>
+    const classes = useStyles();
+    const apiData = store.getState().wells.plotData;
+
+    return (
+    <Dashboard>
+        <Grid container spacing={1} >
+            <Grid item xs={12} md={5} container spacing={2}>
+                <WellsList/>
+                <LogsList/>
+                <FormationsList/>
+                </Grid>
+                <Grid item xs={12} md={7}>
+                    {store.getState().wells.plotData.length !== 0 ?
+                        <div className={classes.plotContainer}>
+                          <WellsPlot plotData={apiData}/>
+                        </div> : 
+                        <div className={classes.logoContainer}>
+                          <EsaLogo />
+                        </div>
+                    }
+                </Grid>
+        </Grid>
+    </Dashboard>
   );
 }
---
+++ src/layouts/components/Plot/WellsPlot.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Plot from 'react-plotly.js';
+
+export const WellsPlot = (props) => {
+    return (
+      <Plot
+        data={props.plotData}
+        useResizeHandler
+        style={{ width: '100%', height: '100%' }}
+        layout={{autosize: true, title: 'Wells Plot'}}
+      />
+    );
+}
